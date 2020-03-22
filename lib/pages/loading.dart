8a4f45fc056be93233bfd7bd3f0758a29a46c40d
Shay de Barra <shaydebarra@gmail.com>
2020-03-22 22:46:09 +0000

    initial commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:worldtime/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  String time = 'loading';
+
+  void setUpWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Dublin', flag: 'ireland.png', url: 'Europe/Dublin');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime' : instance.isDayTime
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setUpWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blueGrey[300],
+        body: Center(
+          child: SpinKitFoldingCube(
+            color: Colors.white,
+            size: 50.0,
+       //     duration: Duration(seconds: 3),
+          ),
+        ));
+  }
+}
